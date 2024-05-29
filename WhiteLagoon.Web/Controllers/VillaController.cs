e3221470570fbd42a64705a87e28cdb705d3ba5e
abdul-aziz <abdulaziz.eu1@gmail.com>
2024-05-29 12:05:33 +0600

    Update And Delete Villa Work

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -26,18 +27,80 @@ namespace WhiteLagoon.Web.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Villa obj)
         {
-            if(obj.Name == obj.Description)
+            try
             {
-                ModelState.AddModelError("", "Villa Name and Drscription Can't be Same!");
-            }
-            if (ModelState.IsValid)
-            {
+                if (obj.Name == obj.Description)
+                {
+                    ModelState.AddModelError("", "Villa Name and Drscription Can't be Same!");
+                }
+                if (ModelState.IsValid)
+                {
 
-                _db.Add(obj);
+                    _db.Add(obj);
+                    _db.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View(obj);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        public IActionResult Update(int id)
+        {
+           
+            Villa? villa = _db.Villas.FirstOrDefault(u=> u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Villa obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Villas.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction(nameof(Index));
-            } 
+            }
             return View(obj);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(u=>u.Id == id);
+
+            if(villa == null) 
+                return NotFound();
+
+            return View(villa);
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Villa villaObj)
+        {
+            try{
+                Villa? villa = _db.Villas.FirstOrDefault(u => u.Id == villaObj.Id);
+                if (villa == null)
+                    return NotFound();
+
+                _db.Villas.Remove(villa);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch{
+                return View(villaObj);
+            }
+
         }
     }
 }
