9560a93b811010d39cdfc5e6343c8866578252ba
abdul-aziz <abdulaziz.eu1@gmail.com>
2024-05-30 10:40:50 +0600

    Index and Create work for VillaNumber Controller, But I need Select for VillaId in Create View

---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumberController (ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(VillaNumber obj)
+        {
+            try
+            {
+                //ModelState.Remove("Villa");
+                if (ModelState.IsValid)
+                {
+
+                    _db.VillaNumbers.Add(obj);
+                    _db.SaveChanges();
+                    TempData["success"] = "New Villa Number Add Successfully";
+                    return RedirectToAction(nameof(Index));
+
+                }
+                TempData["error"] = "Something went Wrong";
+                return View(obj);
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = "Something went Wrong";
+                return View(obj);
+            }
+        }
+
+        public IActionResult Update(int id)
+        {
+           
+            Villa? villa = _db.Villas.FirstOrDefault(u=> u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Villa obj)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Villas.Update(obj);
+                    _db.SaveChanges();
+                    TempData["success"] = "Villa successfully Updated";
+                    return RedirectToAction(nameof(Index));
+                }
+                TempData["error"] = "Something went Wrong";
+                return View(obj);
+            }
+            catch(Exception ex)
+            {
+                TempData["error"] = "Something went Wrong";
+                return View(obj);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Villa? villa = _db.Villas.FirstOrDefault(u=>u.Id == id);
+
+            if(villa == null) 
+                return NotFound();
+
+            return View(villa);
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Villa villaObj)
+        {
+            try{
+                Villa? villa = _db.Villas.FirstOrDefault(u => u.Id == villaObj.Id);
+                if (villa == null)
+                    return NotFound();
+
+                _db.Villas.Remove(villa);
+                _db.SaveChanges();
+                TempData["success"] = "Villa successfully deleted";
+                return RedirectToAction(nameof(Index));
+            }
+            catch{
+                TempData["error"] = "Something went wrong!";
+                return View(villaObj);
+            }
+
+        }
+    }
+}
