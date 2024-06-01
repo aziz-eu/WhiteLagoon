699f6c15205120e47ae2cf867eec7e2dc854d249
abdul-aziz <abdulaziz.eu1@gmail.com>
2024-06-02 00:41:57 +0600

    Add  IRepositoryVilla and Implement in Repository

---
+++ WhiteLagoon.Application/Common/Interface/IVillaRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Common.Interface
+{
+    public interface IVillaRepository
+    {
+        IEnumerable<Villa> Getll(Expression<Func<Villa ,bool>>? filter = null, string? includeProperties = null);
+        IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
+        void Add(Villa Entity);
+        void Update(Villa Entity);
+        void Remove(Villa Entity);
+        void Save();
+    }
+}
---
+++ WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interface;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        public void Add(Villa Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Villa> Getll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Villa Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Villa Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
